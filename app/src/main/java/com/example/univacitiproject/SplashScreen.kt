8bef6ab3d705a77dd65d0015ea593677f8d34c57
Abdulqadri.sanni <abdulqadri08@gmail.com>
2023-10-18 22:51:36 +0100

    Added these to my xml to assist in checking the document on the device - Phase 2

--- app/src/main/java/com/example/univacitiproject/SplashScreen.kt
+++ app/src/main/java/com/example/univacitiproject/SplashScreen.kt
@@ -1,15 +1,19 @@
 package com.example.univacitiproject
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.univacitiproject.databinding.FragmentAddressInfoBinding
 import com.example.univacitiproject.databinding.FragmentSplashScreenBinding
 
 class SplashScreen : Fragment() {
     private lateinit var binding: FragmentSplashScreenBinding
+    var handler: Handler? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,7 +22,21 @@ class SplashScreen : Fragment() {
         val view = inflater.inflate(R.layout.fragment_splash_screen, container, false)
         binding = FragmentSplashScreenBinding.bind(view)
 
+
         return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        nextScreen()
+    }
+
+    private fun nextScreen() {
+        handler = Handler()
+        handler!!.postDelayed({
+            findNavController().navigate(R.id.action_splashScreen_to_signUp)
+        }, 3000)
+    }
+
 
 }
